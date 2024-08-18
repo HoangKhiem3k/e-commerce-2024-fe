d036ae8f933767336f169b2c84da024523472582
hoangkhiem3k <ledohoangkhiem3k@gmail.com>
2024-08-19 01:27:03 +0700

    fix-bug-viewed-product-refactor-authentication

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,25 +1,28 @@
-// ** Config
 import axios from 'axios'
+
+// ** Config
 import { API_ENDPOINT } from 'src/configs/api'
 import instanceAxios from 'src/helpers/axios'
 
-// ** Type
+// ** Types
 import { TLoginAuth, TRegisterAuth, TChangePassword } from 'src/types/auth'
 
 export const loginAuth = async (data: TLoginAuth) => {
+  const res = await axios.post(`${API_ENDPOINT.AUTH.INDEX}/login`, data)
+
+  return res.data
+}
+
+export const logoutAuth = async () => {
   try {
-    const res = await instanceAxios.post(`${API_ENDPOINT.AUTH.INDEX}/login`, data)
+    const res = await instanceAxios.post(`${API_ENDPOINT.AUTH.INDEX}/logout`)
 
     return res.data
   } catch (error) {
     return null
   }
 }
-export const logoutAuth = async () => {
-  const res = await axios.post(`${API_ENDPOINT.AUTH.INDEX}/logout`)
 
-  return res.data
-}
 export const registerAuth = async (data: TRegisterAuth) => {
   try {
     const res = await axios.post(`${API_ENDPOINT.AUTH.INDEX}/register`, data)
@@ -29,6 +32,7 @@ export const registerAuth = async (data: TRegisterAuth) => {
     return error
   }
 }
+
 export const updateAuthMe = async (data: any) => {
   try {
     const res = await instanceAxios.put(`${API_ENDPOINT.AUTH.INDEX}/me`, data)
@@ -48,6 +52,7 @@ export const getAuthMe = async () => {
     return error
   }
 }
+
 export const changePasswordMe = async (data: TChangePassword) => {
   try {
     const res = await instanceAxios.patch(`${API_ENDPOINT.AUTH.INDEX}/change-password`, data)
