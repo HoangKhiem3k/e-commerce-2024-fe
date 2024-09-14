d81d1e623567073a2737afb1db0c9b126418b2ab
hoangkhiem3k <ledohoangkhiem3k@gmail.com>
2024-09-14 22:55:14 +0700

    buy-again-my-order

--- src/views/layouts/components/cart-product/index.tsx
+++ src/views/layouts/components/cart-product/index.tsx
@@ -1,44 +1,31 @@
 // ** React
 import React, { useEffect, useMemo } from 'react'
-
 // ** Next
-import Image from 'next/image'
 import { useRouter } from 'next/router'
-
-// ** Mui Imports
+// ** Mui
 import Box from '@mui/material/Box'
 import Avatar from '@mui/material/Avatar'
 import Menu from '@mui/material/Menu'
 import MenuItem, { MenuItemProps } from '@mui/material/MenuItem'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import Divider from '@mui/material/Divider'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
 import { Badge, Button, Typography, styled, useTheme } from '@mui/material'
-
 // ** Components
 import Icon from 'src/components/Icon'
-
 // ** Hooks
 import { useAuth } from 'src/hooks/useAuth'
-
 // ** Translate
 import { useTranslation } from 'react-i18next'
-
-// ** config
+// ** Config
 import { ROUTE_CONFIG } from 'src/configs/route'
-
 // ** Storage
 import { getLocalProductCart } from 'src/helpers/storage'
-
 // ** Redux
 import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch, RootState } from 'src/stores'
 import { updateProductToCart } from 'src/stores/order-product'
-
 // ** Types
 import { TItemOrderProduct } from 'src/types/order-product'
-
 // ** Utils
 import { formatNumberToLocal, isExpiry } from 'src/utils'
 import NoData from 'src/components/no-data'
@@ -49,8 +36,7 @@ const StyleMenuItem = styled(MenuItem)<MenuItemProps>(({ theme }) => ({}))
 
 const CartProduct = (props: TProps) => {
   // ** Translation
-  const { t, i18n } = useTranslation()
-
+  const { t } = useTranslation()
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
 
   // ** Hooks
@@ -64,7 +50,7 @@ const CartProduct = (props: TProps) => {
 
   const open = Boolean(anchorEl)
 
-  // && Handle
+  // ** Handle
   const handleNavigateDetailsProduct = (slug: string) => {
     router.push(`${ROUTE_CONFIG.PRODUCT}/${slug}`)
   }
@@ -100,6 +86,7 @@ const CartProduct = (props: TProps) => {
 
     return total
   }, [orderItems])
+  console.log('orderItems', { orderItems })
 
   return (
     <React.Fragment>
@@ -158,38 +145,43 @@ const CartProduct = (props: TProps) => {
                 return (
                   <StyleMenuItem key={item.product} onClick={() => handleNavigateDetailsProduct(item.slug)}>
                     <Avatar src={item.image} sx={{ height: '60px !important', width: '60px !important' }} />
-                    <Box>
+                    <Box style={{ flex: 1 }}>
                       <Typography sx={{ maxWidth: '300px', overflow: 'hidden', textOverflow: 'ellipsis' }}>
                         {item.name}
                       </Typography>
-                      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-                        {item.discount > 0 && (
+                      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                          {item.discount > 0 && (
+                            <Typography
+                              variant='h6'
+                              sx={{
+                                color: theme.palette.error.main,
+                                fontWeight: 'bold',
+                                textDecoration: 'line-through',
+                                fontSize: '10px'
+                              }}
+                            >
+                              {formatNumberToLocal(item.price)} VND
+                            </Typography>
+                          )}
                           <Typography
-                            variant='h6'
+                            variant='h4'
                             sx={{
-                              color: theme.palette.error.main,
+                              color: theme.palette.primary.main,
                               fontWeight: 'bold',
-                              textDecoration: 'line-through',
-                              fontSize: '10px'
+                              fontSize: '12px'
                             }}
                           >
-                            {formatNumberToLocal(item.price)} VND
+                            {item.discount > 0 ? (
+                              <>{formatNumberToLocal((item.price * (100 - item.discount)) / 100)}</>
+                            ) : (
+                              <>{formatNumberToLocal(item.price)}</>
+                            )}{' '}
+                            VND
                           </Typography>
-                        )}
-                        <Typography
-                          variant='h4'
-                          sx={{
-                            color: theme.palette.primary.main,
-                            fontWeight: 'bold',
-                            fontSize: '12px'
-                          }}
-                        >
-                          {item.discount > 0 ? (
-                            <>{formatNumberToLocal((item.price * (100 - item.discount)) / 100)}</>
-                          ) : (
-                            <>{formatNumberToLocal(item.price)}</>
-                          )}{' '}
-                          VND
+                        </Box>
+                        <Typography>
+                          x <b>{item.amount}</b>
                         </Typography>
                       </Box>
                     </Box>
