ef41f0c1a3789eb4a8e5b5c97d9f8973239b5b1f
hoangkhiem3k <ledohoangkhiem3k@gmail.com>
2024-10-17 21:57:41 +0700

    update-ui-details-product

--- src/views/pages/product/DetailsProduct/index.tsx
+++ src/views/pages/product/DetailsProduct/index.tsx
@@ -371,7 +371,6 @@ const DetailsProductPage: NextPage<TProps> = () => {
 
     socket.on(ACTION_SOCKET_COMMENT.DELETE_MULTIPLE_COMMENT, data => {
       const deletedCount = deleteManyCommentRecursive(cloneListComment.data, data)
-      console.log('deletedCount', { deletedCount })
       setListComment({
         data: cloneListComment.data,
         total: cloneListComment.total - deletedCount
@@ -477,6 +476,13 @@ const DetailsProductPage: NextPage<TProps> = () => {
     <>
       {loading && <Spinner />}
       <Grid container>
+        <Box marginTop={{ md: 5, xs: 4 }}>
+          <Typography sx={{ color: theme.palette.primary.main, fontWeight: '600', marginBottom: '8px' }}>
+            {t('Product_details')}
+            {' / '}
+            {dataProduct.type?.name} / {dataProduct?.name}
+          </Typography>
+        </Box>
         <Grid
           container
           item
@@ -887,7 +893,8 @@ const DetailsProductPage: NextPage<TProps> = () => {
                     sx={{
                       color: `rgba(${theme.palette.customColors.main}, 0.68)`,
                       fontWeight: 'bold',
-                      fontSize: '18px'
+                      fontSize: '18px',
+                      mb: '8px'
                     }}
                   >
                     {t('Comment_product')} <b style={{ color: theme.palette.primary.main }}>{listComment?.total}</b>{' '}
@@ -895,7 +902,16 @@ const DetailsProductPage: NextPage<TProps> = () => {
                   </Typography>
                   <Box sx={{ width: '100%' }}>
                     <CommentInput onApply={handleComment} />
-                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px', marginTop: '20px' }}>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '8px',
+                        marginTop: '30px',
+                        maxHeight: '500px',
+                        overflow: 'auto'
+                      }}
+                    >
                       {listComment?.data?.map((comment: TCommentItemProduct) => {
                         const level: number = -1
 
@@ -1049,7 +1065,16 @@ const DetailsProductPage: NextPage<TProps> = () => {
               </Typography>
               <Box sx={{ width: '100%' }}>
                 <CommentInput onApply={handleComment} />
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px', marginTop: '20px' }}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '8px',
+                    marginTop: '30px',
+                    maxHeight: '300px',
+                    overflow: 'auto'
+                  }}
+                >
                   {listComment?.data?.map((comment: TCommentItemProduct) => {
                     const level: number = -1
 
