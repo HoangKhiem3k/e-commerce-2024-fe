1bb180b3a99abe5f5da2d7e9e0c7f216503a7342
hoangkhiem3k <ledohoangkhiem3k@gmail.com>
2024-10-19 22:30:29 +0700

    count-order-by-status

--- src/views/pages/dashboard/components/CardCountRevenue.tsx
+++ src/views/pages/dashboard/components/CardCountRevenue.tsx
@@ -1,20 +1,20 @@
 import { Box, useTheme } from '@mui/material'
-import { TCountProductType } from 'src/views/pages/dashboard'
+import { TCountProductType, TCountRevenue } from 'src/views/pages/dashboard'
 import { Bar } from 'react-chartjs-2'
 import 'chart.js/auto'
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
 interface TProps {
-  data: TCountProductType[]
+  data: TCountRevenue[]
 }
-const CardProductType = (props: TProps) => {
+const CardCountRevenue = (props: TProps) => {
   // Props
   const { data } = props
   const theme = useTheme()
   const { t } = useTranslation()
   const labelsMemo = useMemo(() => {
-    return data?.map(item => item?.typeName)
+    return data?.map(item => `${item.month}/${item.year}`)
   }, [data])
   const dataMemo = useMemo(() => {
     return data?.map((item, index) => {
@@ -23,7 +23,7 @@ const CardProductType = (props: TProps) => {
   }, [data])
   const dataSets = [
     {
-      label: `${t('Số lượng')}`,
+      label: `${t('Doanh số')}`,
       backgroundColor: [
         theme.palette.primary.main,
         theme.palette.info.main,
@@ -58,11 +58,11 @@ const CardProductType = (props: TProps) => {
         options={{
           plugins: {
             legend: { display: false },
-            title: { display: true, text: 'Số lượng sản phẩm theo từng loại' }
+            title: { display: true, text: 'Danh số năm nay' }
           }
         }}
       />
     </Box>
   )
 }
-export default CardProductType
+export default CardCountRevenue
--- src/views/pages/dashboard/index.tsx
+++ src/views/pages/dashboard/index.tsx
@@ -8,12 +8,24 @@ import Spinner from 'src/components/spinner'
 import { Box, Grid } from '@mui/material'
 
 // ** Services
-import { getCountAllRecords, getCountProductTypes } from 'src/services/report'
+import {
+  getCountAllRecords,
+  getCountProductStatus,
+  getCountProductTypes,
+  getCountRevenueYear
+} from 'src/services/report'
 import CardCountRecords from 'src/views/pages/dashboard/components/CardCountRecords'
 import CardProductType from 'src/views/pages/dashboard/components/CardProductType'
+import CardCountRevenue from 'src/views/pages/dashboard/components/CardCountRevenue'
 
 export interface TCountProductType {
   typeName: string
+  total: number
+}
+
+export interface TCountRevenue {
+  year: string
+  month: string
   total: number
 }
 
@@ -21,6 +33,7 @@ const Dashboard = () => {
   const [loading, setLoading] = useState(false)
   const [countRecords, setCountRecords] = useState<Record<string, number>>({})
   const [countProductTypes, setCountProductTypes] = useState<TCountProductType[]>([])
+  const [countRevenues, setCountRevenues] = useState<TCountRevenue[]>([])
 
   // ** Fetch API
   const fetchAllCountRecords = async () => {
@@ -49,18 +62,35 @@ const Dashboard = () => {
       })
   }
 
+  const fetchAllTotalRevenues = async () => {
+    setLoading(true)
+    await getCountRevenueYear()
+      .then(res => {
+        const data = res?.data
+        setLoading(false)
+        setCountRevenues(data)
+      })
+      .catch(e => {
+        setLoading(false)
+      })
+  }
+
   useEffect(() => {
     fetchAllCountRecords()
     fetchAllProductTypes()
+    fetchAllTotalRevenues()
   }, [])
 
   return (
     <Box>
       {loading && <Spinner />}
       <CardCountRecords data={countRecords} />
-      <Grid container>
+      <Grid container spacing={6}>
         <Grid item md={6} xs={12}>
           <CardProductType data={countProductTypes} />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <CardCountRevenue data={countRevenues} />
         </Grid>
       </Grid>
     </Box>
