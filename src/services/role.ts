41de51523ede517999dde35b74f6b90abdc23c3c
hoangkhiem3k <ledohoangkhiem3k@gmail.com>
2024-10-28 20:30:48 +0700

    handle-cache-with-tan-stack-react-query

--- src/services/role.ts
+++ src/services/role.ts
@@ -1,54 +1,34 @@
 import { API_ENDPOINT } from 'src/configs/api'
 import instanceAxios from 'src/helpers/axios'
-import { TParamsCreateRole, TParamsDeleteRole, TParamsEditRole, TParamsGetRoles } from 'src/types/role'
+import { TParamsCreateRole, TParamsEditRole, TParamsGetRoles } from 'src/types/role'
 
 export const getAllRoles = async (data: { params: TParamsGetRoles }) => {
-  try {
-    const res = await instanceAxios.get(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}`, data)
+  const res = await instanceAxios.get(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}`, data)
 
-    return res.data
-  } catch (error) {
-    return error
-  }
+  return res.data
 }
 
 export const createRole = async (data: TParamsCreateRole) => {
-  try {
-    const res = await instanceAxios.post(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}`, data)
+  const res = await instanceAxios.post(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}`, data)
 
-    return res.data
-  } catch (error: any) {
-    return error?.response?.data
-  }
+  return res.data
 }
 
 export const updateRole = async (data: TParamsEditRole) => {
   const { id, ...rests } = data
-  try {
-    const res = await instanceAxios.put(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}/${id}`, rests)
+  const res = await instanceAxios.put(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}/${id}`, rests)
 
-    return res.data
-  } catch (error: any) {
-    return error?.response?.data
-  }
+  return res.data
 }
 
 export const deleteRole = async (id: string) => {
-  try {
-    const res = await instanceAxios.delete(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}/${id}`)
+  const res = await instanceAxios.delete(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}/${id}`)
 
-    return res.data
-  } catch (error: any) {
-    return error?.response?.data
-  }
+  return res.data
 }
 
 export const getDetailsRole = async (id: string) => {
-  try {
-    const res = await instanceAxios.get(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}/${id}`)
+  const res = await instanceAxios.get(`${API_ENDPOINT.SYSTEM.ROLE.INDEX}/${id}`)
 
-    return res.data
-  } catch (error: any) {
-    return error?.response?.data
-  }
+  return res.data
 }
